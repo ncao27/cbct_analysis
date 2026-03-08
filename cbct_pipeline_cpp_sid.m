3e4e3811f52bc890ba97a8daa63a4de5cd785b7c
unknown <nathancao522@gmail.com>
2026-03-08 17:51:49 -0500

    added separate version of code that only uses cpp siddon kernel

--- cbct_pipeline_cpp_sid.m
+++ cbct_pipeline_cpp_sid.m
@@ -4,39 +4,6 @@ indir = 'C:\Users\Nathan Cao\OneDrive\Desktop\ct images analysis\cbct_head_phant
 out_filename = '.\projections.raw';
 files = dir([indir '*.IMA']);
 header = dicominfo([indir files(1).name]); % getting the dicom scan information
-nx = single(header.Rows);               % 3D image matrix size along x
-ny = single(header.Columns);            % 3D image matrix size along y
-nz = length(files);                     % 3D image matrix size along z
-dx = single(header.PixelSpacing(1));    % 3D image pixel size. unit: mm
-dy = single(header.PixelSpacing(2));    % 3D image pixel size. unit: mm
-dz = single(header.SliceThickness);     % 3D image pixel size. unit: mm
-sid = 750;                              % Source-to-ISO Distance. unit: mm
-sdd = 1200;                             % Source-to-Detector Distance. unit: mm
-du = 0.154*4;                           % 2D detector pixel size. unit: mm 
-dv = 0.154*4;                           % 2D detector pixel size. unit: mm
-nu = single(2480/4);                    % number of detector pixels along u (native: 1920 × 2480)
-nv = single(1920/4);                    % number of detector pixels along v (native: 1920 × 2480)
-mu_water = 0.02;                        % unit: 1/mm
-
-% we want 20 views, so we get 20 angles from 0 to 360
-n_view = 360; 
-theta_array = linspace(0, 2*pi, n_view);
-
-% define the volume that we store the CT image in 
-vol = zeros(nx, ny, nz, 'double');
-for z = 1:nz
-    image = dicomread([indir files(z).name]);
-    vol(:,:,z) = double(image);
-end
-vol = vol + header.RescaleIntercept;
-vol = (vol + 1000) / 1000 * mu_water;        % this step is to convert the HU to mu. 
-
-%% data importation and declaring necessary varibles
-clear;
-indir = 'C:\Users\Nathan Cao\OneDrive\Desktop\ct images analysis\cbct_head_phantom\DCT_HEAD_CLEAR_NAT_FILL_FULL_HU_NORMAL_[AX3D]_0009\';
-out_filename = '.\projections.raw';
-files = dir([indir '*.IMA']);
-header = dicominfo([indir files(1).name]); % getting the dicom scan information
 nx = single(header.Rows);       % 3D image matrix size along x
 ny = single(header.Columns);    % 3D image matrix size along y
 nz = length(files);             %y 3D image matrix size along z
@@ -97,90 +64,6 @@ mask = ((X - lesion_cx).^2 / lesion_rx^2 + ...
 
 % <=1 makes it logical, convert it to double
 mask = double(mask);
-
-%% Siddon + Projection Domain Lesion Insertion with CUDA Kernel
-mexcuda siddon_kernel.cu
-
-% todo: keep turning everything into a gpuarray on matlab, and then make
-% the necessary changes in cuda. also, this loop where i declare all the
-% variables, maybe i can pre compute all the variables and store into gpu
-% array and then every loop i can just call the gpuarray instead of having
-% to compute and create a separate instance of it, which prolly saves a lot
-% of memory
-
-x_plane = gpuArray(single(((0:nx) - nx/2) * dx)); % note that we go from -nx/2 to +nx/2
-y_plane = gpuArray(single(((0:ny) - ny/2) * dy));
-z_plane = gpuArray(single(((0:nz) - nz/2) * dz));
-projections_vol = zeros(nu, nv, n_view, 'single');
-%projections_les = zeros(nu, nv, n_view, 'single');
-
-% because CUDA converts it to float and vol used to be double
-vol = single(vol);
-gpu_vol = gpuArray(vol);
-
-for view = 1:n_view
-
-    theta = theta_array(view);
-    fprintf('Calculating view #%d/%d (angle = %.0f deg)\n',view, n_view, theta/pi*180);
-    % -------------------------
-    % Source position
-    % -------------------------
-    xs = single(sid * cos(theta));
-    ys = single(sid * sin(theta));
-    zs = single(0);
-
-    % -------------------------
-    % Detector center
-    % -------------------------
-    xd0 = xs - (sdd/sid) * xs;
-    yd0 = ys - (sdd/sid) * ys;
-    zd0 = 0;
-
-    ux = cos(theta - pi/2); %  unit vector
-    uy = sin(theta - pi/2);
-
-    u = 1:nu; % u direction on the detector plane
-    v = 1:nv; % v direction on the detector plan
-
-    % we preallocate the x,y,z coordinates of each detector pixel, and then
-    % loop through them using the cpp kernel
-    xd = single(xd0 + (u - (nu/2+0.5)) * du * ux); 
-    yd = single(yd0 + (u - (nu/2+0.5)) * du * uy);
-    zd = single(zd0 + (v - (nv/2+0.5)) * dv);
-    
-    % C++ kernel doing siddon reconstruction
-    % Inputs:
-    %   - vol: the CT volume that we read in
-    %   - x_plane: the x-dir planes that we define for voxels
-    %   - y_plane: the y-dir planes that we define for voxels
-    %   - z_plane: the z-dir planes that we define for voxels
-    %   - xs: the x-coord of point source
-    %   - ys: the y-coord of point source
-    %   - zs: the z-coord of point source
-    %   - xd: array of x-coordinates for detector plane
-    %   - yd: array of y-coordinates for detector plane
-    %   - zd: array of z-coordinates for detector plane
-    %   - nu: the number of pixels in u direction on detector
-    %   - nv: the number of pixels in v direction on detector
-
-    % find projections of the volume
-    prj_vol = gpuArray.zeros(nu, nv, "single");
-    %prj_les = gpuArray.zeros(nu, nv, "single");
-    siddon_kernel(gpu_vol, prj_vol, x_plane, y_plane, z_plane, ...
-             xs, ys, zs, xd, yd, zd, nu, nv, dx, dy, dz);
-
-    % gather
-    prj_vol = gather(prj_vol);
-    %prj_les = gather(prj_les);
-
-    % store projections
-    projections_vol(:,:,view) = prj_vol;
-    %projections_les(:,:,view) = prj_les;
-
-    if mod(view,10)==0
-        fprintf('Finished view %d/%d\n', view, n_view);
-    end
-end
 
 %% Siddon + Projection Domain Lesion Insertion with C++ Kernel
 
--- siddon_kernel.cu
+++ siddon_kernel.cu
@@ -12,15 +12,15 @@ __global__ void siddon_kernel(
     int nx, int ny, int nz,
     int nu, int nv,
     float dx, float dy, float dz,
-    const double* x_plane,
-    const double* y_plane,
-    const double* z_plane,
-    double xs,
-    double ys,
-    double zs,
-    const double* xd,
-    const double* yd,
-    const double* zd
+    const float* x_plane,
+    const float* y_plane,
+    const float* z_plane,
+    float xs,
+    float ys,
+    float zs,
+    const float* xd,
+    const float* yd,
+    const float* zd
 )
 /*
 Siddon kernel where we will write Siddon reconstruction-related code
@@ -30,41 +30,41 @@ Siddon kernel where we will write Siddon reconstruction-related code
     int i = blockIdx.x * blockDim.x + threadIdx.x;
     int j = blockIdx.y * blockDim.y + threadIdx.y;
 
-    const double x0 = x_plane[0];
-    const double y0 = y_plane[0];
-    const double z0 = z_plane[0];
-
+    const float x0 = x_plane[0];
+    const float y0 = y_plane[0];
+    const float z0 = z_plane[0];
+ 
     // safeguard to stop executing this thread if ix, iy, iz go out of bounds
     if (i >= nu || j >= nv) return;
 
     // find the distance from specific pixel to source
-    double dxr = xd[i] - xs;
-    double dyr = yd[i] - ys;
-    double dzr = zd[j] - zs;
-    double L = sqrt(dxr*dxr + dyr*dyr + dzr*dzr);
+    float dxr = xd[i] - xs;
+    float dyr = yd[i] - ys;
+    float dzr = zd[j] - zs;
+    float L = sqrt(dxr*dxr + dyr*dyr + dzr*dzr);
 
     // check to see if we are out of range or not
     if (fabs(dxr) < 1e-12 || fabs(dyr) < 1e-12 || fabs(dzr) < 1e-12) return;
 
     // calculate the entry and exit of every thread
-    double ax0 = (x_plane[0] - xs) / dxr;
-    double ax1 = (x_plane[nx] - xs) / dxr;
-    double ay0 = (y_plane[0] - ys) / dyr;
-    double ay1 = (y_plane[ny] - ys) / dyr;
-    double az0 = (z_plane[0] - zs) / dzr;
-    double az1 = (z_plane[nz] - zs) / dzr;
+    float ax0 = (x_plane[0] - xs) / dxr;
+    float ax1 = (x_plane[nx] - xs) / dxr;
+    float ay0 = (y_plane[0] - ys) / dyr;
+    float ay1 = (y_plane[ny] - ys) / dyr;
+    float az0 = (z_plane[0] - zs) / dzr;
+    float az1 = (z_plane[nz] - zs) / dzr;
 
-    double a_min = fmax(fmax(fmin(ax0, ax1), fmin(ay0, ay1)), fmin(az0, az1));
-    double a_max = fmin(fmin(fmax(ax0, ax1), fmax(ay0, ay1)), fmax(az0, az1));
+    float a_min = fmax(fmax(fmin(ax0, ax1), fmin(ay0, ay1)), fmin(az0, az1));
+    float a_max = fmin(fmin(fmax(ax0, ax1), fmax(ay0, ay1)), fmax(az0, az1));
 
     if (a_min >= a_max) return;
 
     // -----------------------------
     // Initial voxel indices
     // -----------------------------
-    double xm = xs + a_min * dxr;
-    double ym = ys + a_min * dyr;
-    double zm = zs + a_min * dzr;
+    float xm = xs + a_min * dxr;
+    float ym = ys + a_min * dyr;
+    float zm = zs + a_min * dzr;
 
     int ix = (int)floor((xm - x0) / dx);
     int iy = (int)floor((ym - y0) / dy);
@@ -85,22 +85,22 @@ Siddon kernel where we will write Siddon reconstruction-related code
     // -----------------------------
     // Next boundary distances
     // -----------------------------
-    double ax = (x_plane[ix + (sx > 0)] - xs) / dxr;
-    double ay = (y_plane[iy + (sy > 0)] - ys) / dyr;
-    double az = (z_plane[iz + (sz > 0)] - zs) / dzr;
+    float ax = (x_plane[ix + (sx > 0)] - xs) / dxr;
+    float ay = (y_plane[iy + (sy > 0)] - ys) / dyr;
+    float az = (z_plane[iz + (sz > 0)] - zs) / dzr;
 
-    double dax = dx / fabs(dxr);
-    double day = dy / fabs(dyr);
-    double daz = dz / fabs(dzr);
+    float dax = dx / fabs(dxr);
+    float day = dy / fabs(dyr);
+    float daz = dz / fabs(dzr);
 
     // -----------------------------
     // Siddon traversal
     // -----------------------------
-    double a = a_min;
-    double sum = 0.0;
+    float a = a_min;
+    float sum = 0.0;
 
     while (a < a_max) {
-        double a_next;
+        float a_next;
 
         if (ax <= ay && ax <= az) {
             a_next = ax;
@@ -120,7 +120,7 @@ Siddon kernel where we will write Siddon reconstruction-related code
             iy >= 0 && iy < (int)ny &&
             iz >= 0 && iz < (int)nz) {
 
-            double len = (a_next - a) * L;
+            float len = (a_next - a) * L;
             sum += vol[ix + iy*nx + iz*nx*ny] * len;
         }
 
@@ -129,7 +129,6 @@ Siddon kernel where we will write Siddon reconstruction-related code
 
     proj[(int) (i + j*nu)] = sum;
 }
-
 
 void mexFunction(int nlhs, mxArray *plhs[], 
     int nrhs, const mxArray *prhs[])
@@ -150,51 +149,32 @@ void mexFunction(int nlhs, mxArray *plhs[],
     int nz = (int)mxGetDimensions(prhs[0])[2];
 
     // get the x_plane, y_plane, and z_plane
-    const double* x_plane = mxGetPr(prhs[2]);
-    const double* y_plane = mxGetPr(prhs[3]);
-    const double* z_plane = mxGetPr(prhs[4]);
+    const float* x_plane = (const float*) mxGetData(prhs[2]);
+    const float* y_plane = (const float*) mxGetData(prhs[3]);
+    const float* z_plane = (const float*) mxGetData(prhs[4]);
 
     // get the source coordinates
-    double xs = static_cast<double>(mxGetScalar(prhs[5]));
-    double ys = static_cast<double>(mxGetScalar(prhs[6]));
-    double zs = static_cast<double>(mxGetScalar(prhs[7]));
+    float xs = static_cast<float>(mxGetScalar(prhs[5]));
+    float ys = static_cast<float>(mxGetScalar(prhs[6]));
+    float zs = static_cast<float>(mxGetScalar(prhs[7]));
 
     //get the detector coordinates
-    const double* xd = mxGetPr(prhs[8]);
-    const double* yd = mxGetPr(prhs[9]);
-    const double* zd = mxGetPr(prhs[10]);
+    const float* xd = (const float*) mxGetData(prhs[8]);
+    const float* yd = (const float*) mxGetData(prhs[9]);
+    const float* zd = (const float*) mxGetData(prhs[10]);
 
     // get the number of detector plane grid points
     int nu = (int) mxGetScalar(prhs[11]);
     int nv = (int) mxGetScalar(prhs[12]);
 
     // get the grid point sizes of the volume
-    const double dx = mxGetScalar(prhs[13]);
-    const double dy = mxGetScalar(prhs[14]);
-    const double dz = mxGetScalar(prhs[15]);
+    const float dx = mxGetScalar(prhs[13]);
+    const float dy = mxGetScalar(prhs[14]);
+    const float dz = mxGetScalar(prhs[15]);
 
-    // we have to make the matlab cpu arrays into gpu arrays
-    double *d_x_plane, *d_y_plane, *d_z_plane;
-    double *d_xd, *d_yd, *d_zd;
-
-    cudaMalloc(&d_x_plane, (nx+1)*sizeof(double));
-    cudaMalloc(&d_y_plane, (ny+1)*sizeof(double));
-    cudaMalloc(&d_z_plane, (nz+1)*sizeof(double));
-
-    cudaMemcpy(d_x_plane, x_plane, (nx+1)*sizeof(double), cudaMemcpyHostToDevice);
-    cudaMemcpy(d_y_plane, y_plane, (ny+1)*sizeof(double), cudaMemcpyHostToDevice);
-    cudaMemcpy(d_z_plane, z_plane, (nz+1)*sizeof(double), cudaMemcpyHostToDevice);
-
-
-    cudaMalloc(&d_xd, nu*sizeof(double));
-    cudaMalloc(&d_yd, nu*sizeof(double));
-    cudaMalloc(&d_zd, nv*sizeof(double));
-
-    cudaMemcpy(d_xd, xd, nu*sizeof(double), cudaMemcpyHostToDevice);
-    cudaMemcpy(d_yd, yd, nu*sizeof(double), cudaMemcpyHostToDevice);
-    cudaMemcpy(d_zd, zd, nv*sizeof(double), cudaMemcpyHostToDevice);
-
-
+    cudaMemcpy(d_xd, xd, nu*sizeof(float), cudaMemcpyHostToDevice);
+    cudaMemcpy(d_yd, yd, nu*sizeof(float), cudaMemcpyHostToDevice);
+    cudaMemcpy(d_zd, zd, nv*sizeof(float), cudaMemcpyHostToDevice);
 
     // we now define the number of threads and number of blocks used
     // matlab goes though each view, so realistically we only worry about one panel
@@ -211,9 +191,9 @@ void mexFunction(int nlhs, mxArray *plhs[],
         nx, ny, nz,
         nu, nv,
         dx, dy, dz,
-        d_x_plane, d_y_plane, d_z_plane,
+        x_plane, y_plane, z_plane,
         xs, ys, zs,
-        d_xd, d_yd, d_zd
+        xd, yd, zd
     );
 
     // get real time feedback of where the error comes from
@@ -224,4 +204,5 @@ void mexFunction(int nlhs, mxArray *plhs[],
         mexErrMsgIdAndTxt("CUDA:siddon_kernel",
             cudaGetErrorString(err));
     }
+
 }
